3cfd82da6869be213ed6f20e78b662296d528564
luis M Ponce <luisponcekun@gmail.com>
2016-03-04 10:30:26 -0500

    super basic session control

--- Server.js
+++ Server.js
@@ -1,5 +1,7 @@
 var express =   require("express");
 var multer  =   require('multer');
+var bodyParser = require('body-parser');
+var session = require('express-session');
 var app         =   express();
 var storage =   multer.diskStorage({
 	destination: function (req, file, callback) {
@@ -11,10 +13,42 @@ var storage =   multer.diskStorage({
 });
 var upload = multer({ storage : storage}).single('userPhoto');
 
-app.use(express.static(__dirname+'/'));
+app.set('views', __dirname + '/views');
+app.use(express.static(__dirname+'/resources'));
+app.engine('html', require('ejs').renderFile);
+
+app.use(session({secret: 'ssshhhhh'}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+var sess;
+
+
 
 app.get('/',function(req,res){
-	res.sendFile(__dirname + "/index.html");
+	sess = req.session;
+	if(sess.email){
+		res.redirect('/home');
+	} else {
+		res.render('login.html');
+	}
+});
+
+app.get('/home', function(req, res){
+	sess = req.session;
+	if(sess.email){
+		res.render('index.html');
+	} else {
+		res.write('<h1>Please login first.</h1>');
+		res.end('<a href="/">Login</a>');
+	}
+});
+
+app.post('/login', function(req,res){
+	sess=req.session;
+
+	sess.email=req.body.email;
+	res.end('done');
 });
 
 app.post('/api/photo',function(req,res){
@@ -31,6 +65,17 @@ app.get('/uploads/:name', function(req, res){
 	res.sendFile(filename, {root: './uploads'});
 });
 
+app.get('/logout',function(req,res){
+	req.session.destroy(function(err){
+		if(err){
+			console.log(err);
+		} else {
+			res.redirect('/');
+		}
+	});
+});
+
 app.listen(3005,function(){
 	console.log("Working on port 3005");
 });
+
