d6526b4cdd5eba61a59c3d110cb91c2245717055
luis M Ponce <luisponcekun@gmail.com>
2016-03-03 20:02:56 -0500

    Server now serving images on ./uploads

--- Server.js
+++ Server.js
@@ -26,6 +26,11 @@ app.post('/api/photo',function(req,res){
 	});
 });
 
+app.get('/uploads/:name', function(req, res){
+	var filename = req.params.name;
+	res.sendFile(filename, {root: './uploads'});
+});
+
 app.listen(3005,function(){
 	console.log("Working on port 3005");
 });
