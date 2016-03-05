76820589e1a1ef66bd48fb3b4a5ac91eab096d62
Luis Ponce <luisponcekun@gmail.com>
2016-03-05 18:21:25 -0500

    working on mongo connection

--- DBController.js
+++ DBController.js
@@ -1,22 +1,24 @@
 var mongoose = require('mongoose');
-
-exports.connect = function(callback){
+exports.mongoose;
+exports.connect = function(){
     mongoose.connect('mongodb://localhost/superfotos');
     var db = mongoose.connection;
     db.on('error', console.error.bind(console, 'connection error:'));
-    db.once('open', callback);
+    db.once('open', function(){
+	console.log('db connected');
+    });
 }
 
 //user schema
-var userSchema = new mongoose.Schema({
+var userSchema = mongoose.Schema({
 	name: String,
 	username: String,
 	password: String
-
 });
 
-exports.User = User;
+
 var User = mongoose.model('User', userSchema);
+exports.User = User;
 
 // User.statics.findByUsername = function(usrname, cb){
 // 	return this.find({username: usrname}, cb);
