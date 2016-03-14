3d6833c6ca9839e704dcd5bfa00d6bb677f3dbe5
luis M ponce de leon <luisponcekun@gmail.com>
2016-03-14 18:30:27 -0500

    Fix #10 : Cambiar espacios por %20 en URIs de las fotos de los posts

--- Server.js
+++ Server.js
@@ -1,24 +1,24 @@
-var express 	= 	require("express");
-var multer  	=  require('multer');
-var bodyParser = 	require('body-parser');
-var session 	= 	require('express-session');
+var express = require("express");
+var multer = require('multer');
+var bodyParser = require('body-parser');
+var session = require('express-session');
 var DBController = require('./DBController');
-var encrypter 	=  require('./helpers/passEncription');
+var encrypter = require('./helpers/passEncription');
 var fs = require('fs');
-var app 			= 	express();
-var storage 	=  multer.diskStorage({
-	destination: function (req, file, callback) {
-		callback(null, './uploads');
-	},
-	filename: function (req, file, callback) {
-		callback(null, req.session.usr+"-"+Date.now());
-	}
+var app = express();
+var storage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, './uploads');
+    },
+    filename: function (req, file, callback) {
+        callback(null, req.session.usr + "-" + Date.now());
+    }
 });
-var upload = multer({ storage : storage}).single('userPhoto');
+var upload = multer({storage: storage}).single('userPhoto');
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
-app.use(express.static(__dirname+'/resources'));
+app.use(express.static(__dirname + '/resources'));
 app.engine('html', require('ejs').renderFile);
 
 app.use(session({secret: 'ssshhhhh'}));
@@ -27,235 +27,239 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 var sess;
 
-app.get('/',function(req,res){
-	sess = req.session;
-	if(sess.usr){
-		res.redirect('/home');
-	} else {
-		res.render('login.html');
-	}
+app.get('/', function (req, res) {
+    sess = req.session;
+    if (sess.usr) {
+        res.redirect('/home');
+    } else {
+        res.render('login.html');
+    }
 });
 
-app.get('/signup',function(req,res){
-	sess = req.session;
-	if(sess.usr){
-		res.redirect('/home');
-	} else {
-		res.render('signup.html');
-	}
+app.get('/signup', function (req, res) {
+    sess = req.session;
+    if (sess.usr) {
+        res.redirect('/home');
+    } else {
+        res.render('signup.html');
+    }
 });
 
-app.get('/home', function(req, res){
-	sess = req.session;
-	if(sess.usr){
-		res.render('index.html');
-	} else {
-		res.redirect('/');
-	}
+app.get('/home', function (req, res) {
+    sess = req.session;
+    if (sess.usr) {
+        res.render('index.html');
+    } else {
+        res.redirect('/');
+    }
 });
 
-app.post('/register', function(req,res){
-	sess = req.session;
-	if(sess.usr){
-		//TODO user already logged in
-	} //else {
-		//res.redirect('/signup');
-	//}
+app.post('/register', function (req, res) {
+    sess = req.session;
+    if (sess.usr) {
+        //TODO user already logged in
+    } //else {
+    //res.redirect('/signup');
+    //}
 
-	encrypter.cryptPassword(req.body.password, function(err, hash){
-		if(err){
-			//TODO error handling
-		}
-		var user = {
-			name: req.body.name,
-			username: req.body.username,
-			password: hash
-		};
+    encrypter.cryptPassword(req.body.password, function (err, hash) {
+        if (err) {
+            //TODO error handling
+        }
+        var user = {
+            name: req.body.name,
+            username: req.body.username,
+            password: hash
+        };
 
-		var userInstance = new DBController.User(user);
-		userInstance.save(function(err, userInstance){
-			if(err) {
-				console.log(err);
-				res.end(err);
-			} else {
-				sess.usr = user.name;
-				res.redirect('/');
-				res.end('done');
-			}
-		});
-	});
+        var userInstance = new DBController.User(user);
+        userInstance.save(function (err, userInstance) {
+            if (err) {
+                console.log(err);
+                res.end(err);
+            } else {
+                sess.usr = user.name;
+                res.redirect('/');
+                res.end('done');
+            }
+        });
+    });
 });
 
-app.post('/login', function(req,res){
-	sess=req.session;
-	if(sess.usr){
-		//TODO user already logged in
-	}
+app.post('/login', function (req, res) {
+    sess = req.session;
+    if (sess.usr) {
+        //TODO user already logged in
+    }
 
-   DBController.User
-   	.findOne({'username': req.body.username}, function(err, user){
-			if(err) res.end(err);
-			if(user==null){
-				res.end('wrong username');
-			} else {
-				encrypter
-				.comparePassword(req.body.password, user.password, function(err, isMatch){
-					if(err) res.end(err);
-					if(isMatch){
-						sess.usr = user.username;
-						res.redirect('/');
-						res.end('done');
-					} else {
-						res.end('wrong password')
-					}
-				});
-			}
-		});
+    DBController.User
+        .findOne({'username': req.body.username}, function (err, user) {
+            if (err)
+                res.end(err);
+            if (user === null) {
+                res.end('wrong username');
+            } else {
+                encrypter
+                    .comparePassword(req.body.password, user.password, function (err, isMatch) {
+                        if (err)
+                            res.end(err);
+                        if (isMatch) {
+                            sess.usr = user.username;
+                            res.redirect('/');
+                            res.end('done');
+                        } else {
+                            res.end('wrong password')
+                        }
+                    });
+            }
+        });
 });
 
-app.post('/post', upload, function(req,res, next){
-	var body = req.body;
+app.post('/post', upload, function (req, res, next) {
+    var body = req.body;
 
-	sess=req.session;
-	if(sess.usr == null){
-		res.end('Access Denied');
-	} else {
-		DBController.User
-		.findOne({'username': sess.usr}, function(err, user){
-			if(err) {
-				//TODO error handling
-				console.log(err);
-			} else
+    sess = req.session;
+    if (sess.usr === null) {
+        res.end('Access Denied');
+    } else {
+        DBController.User
+            .findOne({'username': sess.usr}, function (err, user) {
+                if (err) {
+                    //TODO: error handling
+                    console.log(err);
+                } else
+                    upload(req, res, function (err) {
+                        if (err) {
+                            return res.end(err);
+                        } else {
+                            // req.file is the file
+                            // req.body will hold the text fields, if there were any
+                            var tagarr = new Array();
 
-			upload(req,res, function(err) {
-				if(err) {
-					return res.end(err);
-				} else {
-					// req.file is the file
-	  				// req.body will hold the text fields, if there were any
-	  				var tagarr = new Array();
+                            // tagarr = body.taglist.split(',');
 
-	  				// tagarr = body.taglist.split(',');
+                            var urlFriendlyPostName = body.postname.replace(' ', '%20');
 
-	  				var post = {
-	  					owner: user._id,
-	  					title: body.postname,
-	  					image: {
-	  						name: body.imagename,
-	  						filename: req.file.filename,
-	  						uri: "/post/"+body.postname+"/photo"
-	  					},
-	  					description: body.description
-	  				}
+                            var post = {
+                                owner: user._id,
+                                title: body.postname,
+                                image: {
+                                    name: body.imagename,
+                                    filename: req.file.filename,
+                                    uri: "/post/" + urlFriendlyPostName + "/photo"
+                                },
+                                description: body.description
+                            }
 
-	  				var postInstance = new DBController.Post(post);
+                            var postInstance = new DBController.Post(post);
 
-	  				postInstance.save(function(err, postInstance){
-	  					if(err){
-	  						//TODO error handling
-	  					} else {
-	  						// crearTags(tagarr, postInstance, function(postInstance){
-			  				// 	res.end('created');
-			  				// });
-	  						user.posts.push(postInstance);
-	  						user.save();
+                            postInstance.save(function (err, postInstance) {
+                                if (err) {
+                                    //TODO error handling
+                                } else {
+                                    // crearTags(tagarr, postInstance, function(postInstance){
+                                    // 	res.end('created');
+                                    // });
+                                    user.posts.push(postInstance);
+                                    user.save();
 
-	  						res.redirect('/myposts');
-	  					}
-	  				});
-				}
-			});
-		});
-	}
+                                    res.redirect('/myposts');
+                                }
+                            });
+                        }
+                    });
+            });
+    }
 });
 
-function crearTags(tagarr, postInstance, cb){
-	for (var i = tagarr.length - 1; i >= 0; i--) {
-		var tagInstance = new DBController.Tag({name: tagarr[i]});
-		tagInstance.save(function(err, tag){
-			if (err) {
-				//TODO error handling
-			}
-			postInstance.tags.push(tagInstance);
-			postInstance.save();
-		});
-	};
+function crearTags(tagarr, postInstance, cb) {
+    for (var i = tagarr.length - 1; i >= 0; i--) {
+        var tagInstance = new DBController.Tag({name: tagarr[i]});
+        tagInstance.save(function (err, tag) {
+            if (err) {
+                //TODO error handling
+            }
+            postInstance.tags.push(tagInstance);
+            postInstance.save();
+        });
+    }
+    ;
 
-	return cb(postInstance);
+    return cb(postInstance);
 }
 
-app.get('/myposts', function(req, res){
-	sess = req.session;
-	if(sess.usr == null){
-		res.end('Access Denied');
-	} else {
-		DBController.User
-		.findOne({'username': sess.usr})
-		.populate('posts')
-		.exec(function(err, user){
-			if(err){
-				console.log(err);
-			} else {
-				if(user.posts){
-    				res.render('listMyPosts', {
-						misposts: user.posts
-					});
-    			} else {
-    				res.end();
-    			}
-			}
+app.get('/myposts', function (req, res) {
+    sess = req.session;
+    if (sess.usr === null) {
+        res.redirect('/');
+    } else {
+        DBController.User
+            .findOne({'username': sess.usr})
+            .populate('posts')
+            .exec(function (err, user) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    if (user.posts) {
+                        res.render('listMyPosts', {
+                            misposts: user.posts
+                        });
+                    } else {
+                        res.end();
+                    }
+                }
 
-		});
-	}
+            });
+    }
 });
 
-app.get('/post/:title', function(req,res){
-	var title = req.params.title;
+app.get('/post/:title', function (req, res) {
+    var title = req.params.title;
 
-	//TODO: validate user logged in is owner of post
-	DBController.Post
-	.findOne({'title':title}, function(err,post){
-		if(err){
-			res.end(err);
-		} else {
-			res.end(JSON.stringify(post));
-		}
-	});
+    //TODO: validate user logged in is owner of post
+    DBController.Post
+        .findOne({'title': title}, function (err, post) {
+            if (err) {
+                res.end(err);
+            } else {
+                res.end(JSON.stringify(post));
+            }
+        });
 });
 
-app.get('/post/:title/photo', function(req,res){
-	var title = req.params.title;
+app.get('/post/:title/photo', function (req, res) {
+    var title = req.params.title;
 
-	DBController.Post.findOne({'title':title}, function(err,post){
-		if(err){
-			res.end(err);
-		} else {
-			var filename = post.image.filename;
+    DBController.Post.findOne({'title': title}, function (err, post) {
+        if (err) {
+            res.end(err);
+        } else {
+            var filename = post.image.filename;
 
-			var img = fs.readFileSync('./uploads/'+filename);
-	     	res.writeHead(200, {'Content-Type': 'image/png' });
-	     	res.end(img, 'binary');
+            var img = fs.readFileSync('./uploads/' + filename);
+            res.writeHead(200, {'Content-Type': 'image/png'});
+            res.end(img, 'binary');
 
-			// res.sendFile(filename, {root: './uploads'});
-		}
-	});
+            // res.sendFile(filename, {root: './uploads'});
+        }
+    });
 });
 
-app.get('/uploads/:name', function(req, res){
-	var filename = req.params.name;
-	res.sendFile(filename, {root: './uploads'});
+app.get('/uploads/:name', function (req, res) {
+    var filename = req.params.name;
+    res.sendFile(filename, {root: './uploads'});
 });
 
-app.get('/logout',function(req,res){
-	req.session.destroy(function(err){
-		if(err){
-			console.log(err);
-		} else {
-			res.redirect('/');
-		}
-	});
+app.get('/logout', function (req, res) {
+    req.session.destroy(function (err) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.redirect('/');
+        }
+    });
 });
 
-app.listen(3005,function(){
-	console.log("Working on port 3005");
+app.listen(3005, function () {
+    console.log("Working on port 3005");
 });
